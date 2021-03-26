909495b0708074d9f5f96fd60a50ddd4d826e4cd
Daniel Astudillo <ret640@gmail.com>
2021-03-26 11:31:24 -0400

    changed plotting into a function

--- src/plotting.py
+++ src/plotting.py
@@ -17,6 +17,27 @@ def read_data(filename,delimiter=',',starting_row=0):
     temperature_data = np.array(all_data[starting_row:,:], dtype=float)
     return temperature_data
 
+def figure_create(data):
+    """Processes a data file with the processed_temperature_data structure and plots the first and third columns
+    and also saves the figure as pdf and the datas as a .json file"""
+    temperature_figure = plt.figure()
+    temperature_plot = plt.bar (data[:,0],data[:,2], width=35, color='blue')
+
+
+    temperature_figure.savefig('results/temperature-over-time.pdf')
+
+    all_data = pd.read_csv("data/110-tavg-12-12-1950-2020.csv", index_col='Date', header=4)
+    all_data.info()
+    all_data.to_json("results/data_output.json")
+    plt.show()
+
+
+    json_data = pd.read_json("results/data_output.json")
+    json_data.info()
+
+    temperature_plot = plt.bar (all_data.loc[:,"Value"], height=all_data.loc[:,"Value"], width=30)
+    plt.show()
+
 temperature_data = read_data("data/110-tavg-12-12-1950-2020.csv", starting_row=5)
 
 # Compute a new column by multiplying column number 1 to Kelvin
@@ -25,22 +46,8 @@ temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
 # Append this new column to the existing temperature_data array
 processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
 
-# Create a figure of the processed data
-temperature_figure = plt.figure()
-temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=35, color='blue')
-
-plt.show(block=True)
-temperature_figure.savefig('results/temperature-over-time.pdf')
-
-all_data = pd.read_csv("data/110-tavg-12-12-1950-2020.csv", index_col='Date', header=4)
-all_data.info()
-all_data.to_json("results/data_output.json")
+figure_create(processed_temperature_data)
 
 
 
-json_data = pd.read_json("results/data_output.json")
-json_data.info()
-
-temperature_plot = plt.bar (all_data.loc[:,"Value"], height=all_data.loc[:,"Value"], width=30)
-plt.show(block=True)
 
