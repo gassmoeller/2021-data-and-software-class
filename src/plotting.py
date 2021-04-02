6088d3b8eb4b3e467c4545c65cf14a932437f9e3
Daniel Astudillo <ret640@gmail.com>
2021-04-02 19:36:38 -0400

    added folder management and sys.argv

--- src/plotting.py
+++ src/plotting.py
@@ -5,6 +5,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import sys
 
 # Create a function to read the data file
 def read_data(filename,delimiter=',',starting_row=0):
@@ -17,36 +19,59 @@ def read_data(filename,delimiter=',',starting_row=0):
     temperature_data = np.array(all_data[starting_row:,:], dtype=float)
     return temperature_data
 
-def figure_create(data):
+def figure_create(data,results_filename,input_filename,results_plot):
     """Processes a data file with the processed_temperature_data structure and plots the first and third columns
     and also saves the figure as pdf and the datas as a .json file"""
     temperature_figure = plt.figure()
     temperature_plot = plt.bar (data[:,0],data[:,2], width=35, color='blue')
 
 
-    temperature_figure.savefig('results/temperature-over-time.pdf')
+    temperature_figure.savefig(results_plot)
 
-    all_data = pd.read_csv("data/110-tavg-12-12-1950-2020.csv", index_col='Date', header=4)
+    all_data = pd.read_csv(input_filename, index_col='Date', header=4)
     all_data.info()
-    all_data.to_json("results/data_output.json")
+    all_data.to_json(results_filename)
     plt.show()
 
 
-    json_data = pd.read_json("results/data_output.json")
+    json_data = pd.read_json(results_filename)
     json_data.info()
 
     temperature_plot = plt.bar (all_data.loc[:,"Value"], height=all_data.loc[:,"Value"], width=30)
     plt.show()
 
-temperature_data = read_data("data/110-tavg-12-12-1950-2020.csv", starting_row=5)
+def initialize(inputs="110-tavg-12-12-1950-2020.csv"):
+    input_file = inputs
+    plot_file = "temperature-over-time.pdf"
+    json_output_file="data_output.json"
 
-# Compute a new column by multiplying column number 1 to Kelvin
-temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
 
-# Append this new column to the existing temperature_data array
-processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
+    data_directory=os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
+    results_directory=os.path.realpath(os.path.join(os.path.dirname(__file__),"..","results"))
 
-figure_create(processed_temperature_data)
+    input_filename= os.path.join(data_directory,input_file)
+    plot_filename= os.path.join(results_directory,plot_file)
+    json_filename= os.path.join(results_directory,json_output_file)
+
+    temperature_data = read_data(input_filename, starting_row=5)
+
+    # Compute a new column by multiplying column number 1 to Kelvin
+    temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
+
+    # Append this new column to the existing temperature_data array
+    processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
+
+    figure_create(processed_temperature_data,json_filename,input_filename,plot_filename)
+
+
+if __name__=="__main__":
+    if len(sys.argv)==1:
+        initialize()
+    elif len(sys.argv)==2:
+        initialize(inputs=sys.argv[1])
+    else:
+        print("too many arguments")
+        
 
 
 
