f38d9d30903f80a313b983b046c3eccbabf9dae8
Mackenzie Ross <rossma.52@ufl.edu>
2021-04-02 11:25:32 -0400

    Staged changes

--- src/plotting.py
+++ src/plotting.py
@@ -6,29 +6,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Create a function to read the data file
-def read_data(filename,delimiter=',',starting_row=0):
-    """This function reads data from a specified filename. 
-    The specified filename should point to a .csv file."""
-    # Create an array (a multi-dimensional table) out of our data file, full of text
-    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=5)
+# Create an array (a multi-dimensional table) out of our data file, full of text
+all_data = np.genfromtxt("data/110-tavg-12-12-1950-2020.csv", delimiter=',',skip_header=5)
 
-    # Select the data range we are interested in, convert it into a new array, full of numbers
-    temperature_data = np.array(all_data[starting_row:,:], dtype=float)
-    return temperature_data
-
-temperature_data = read_data("data/110-tavg-12-12-1950-2020.csv", starting_row=5)
+# Select the data range we are interested in, convert it into a new array, full of numbers
+temperature_data = np.array(all_data[5:,:], dtype=float)
 
 # Compute a new column by multiplying column number 1 to Kelvin
 temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
 
 # Append this new column to the existing temperature_data array
 processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
+print (processed_temperature_data)
 
 # Create a figure of the processed data
 temperature_figure = plt.figure()
-temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=35, color='blue')
-
+temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=30, color='blue')
 plt.show(block=True)
 temperature_figure.savefig('results/temperature-over-time.pdf')
 
@@ -36,7 +29,7 @@ all_data = pd.read_csv("data/110-tavg-12-12-1950-2020.csv", index_col='Date', he
 all_data.info()
 all_data.to_json("results/data_output.json")
 
-
+print("hello world", all_data.loc['195012':'197512','Value'])
 
 json_data = pd.read_json("results/data_output.json")
 json_data.info()
