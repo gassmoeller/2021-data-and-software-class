013b66a05e19f45944821abb64a970300d3680ec
Mackenzie Ross <rossma.52@ufl.edu>
2021-04-02 15:43:55 -0500

    Added csv file

--- src/plotting.py
+++ src/plotting.py
@@ -9,39 +9,41 @@ import os
 import sys
 
 # Create an array (a multi-dimensional table) out of our data file, full of text
-all_data = np.genfromtxt("data/110-tavg-12-12-1950-2020.csv", delimiter=',',skip_header=5)
+all_data = np.genfromtxt("stable_isotope_data_assignment2 - Sheet1.csv", delimiter=',',skip_header=3)
 
 # Select the data range we are interested in, convert it into a new array, full of numbers
-temperature_data = np.array(all_data[5:,:], dtype=float)
+temperature_data = np.array(all_data[3:,:], dtype=float)
 
-    # Append this new column to the existing temperature_data array
-    processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
-    return processed_temperature_data
+    # Select the data range we are interested in, convert it into a new array, full of numbers
+isotope_data = np.array(all_data[3:,:], dtype=float)
+    return temperature_data
 
-# Append this new column to the existing temperature_data array
-processed_temperature_data = np.append(temperature_data, temperature_kelvin,1)
-print (processed_temperature_data)
+# Append this new column to the existing isotope_data array
+processed_isotope_data = np.append(isotope_data, isotope_data,1)
+print (processed_isotope_data)
 
 # Create a figure of the processed data
-temperature_figure = plt.figure()
-temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=30, color='blue')
+plt.style.use('seaborn-whitegrid')
+isotope_figure = plt.figure()
+isotope_plot = plt.scatter (processed_isotope_data[:,0], processed_isotope_data[:,2])
+plt.xlabel("Carbon")
+plt.ylabel("Oxygen")
+plt.title('Stable Isotope Analysis')
 plt.show(block=True)
-temperature_figure.savefig('results/temperature-over-time.pdf')
+isotope_figure.savefig('results/stable-isotopes.pdf')
 
 def convert_data(filename, output_filename):
     all_data = pd.read_csv(filename, index_col='Date', header=4)
     all_data.info()
     all_data.to_json(output_filename)
 
-print("hello world", all_data.loc['195012':'197512','Value'])
-
     input_filename = os.path.join(data_directory,input_file)
     plot_filename = os.path.join(results_directory,plot_file)
     json_filename = os.path.join(results_directory,json_output_file)
 
-    temperature_data = read_data(input_filename, starting_row=5)
-    processed_temperature_data = process_data(temperature_data)
-    plot_data(processed_temperature_data, plot_filename)
+    isotope_data = read_data(input_filename, starting_row=3)
+    processed_isotope_data = process_data(isotope_data)
+    plot_data(processed_isotope_data, plot_filename)
     convert_data(input_filename, json_filename)
 
 if __name__ == "__main__":
