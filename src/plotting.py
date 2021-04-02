7fb09089c54604133f85c0199ade1b01ca0e77e8
Mackenzie Ross <rossma.52@ufl.edu>
2021-04-02 16:28:36 -0500

    Altered plotting script

--- src/plotting.py
+++ src/plotting.py
@@ -9,28 +9,24 @@ import os
 import sys
 
 # Create an array (a multi-dimensional table) out of our data file, full of text
-all_data = np.genfromtxt("stable_isotope_data_assignment2-Sheet1.csv", delimiter=',',skip_header=3)
+all_data = np.genfromtxt("110-tavg-12-12-1950-2020.csv", delimiter=',',skip_header=)
 
 # Select the data range we are interested in, convert it into a new array, full of numbers
-isotope_data = np.array(all_data[3:,:], dtype=float)
+temperature_data = np.array(all_data[3:,:], dtype=float)
 
  # Select the data range we are interested in, convert it into a new array, full of numbers
-isotope_data = np.array(all_data[3:,:], dtype=float)
-print(isotope_data)
+temperature_data = np.array(all_data[3:,:], dtype=float)
+print(temperature_data)
 
-# Append this new column to the existing isotope_data array
-processed_isotope_data = np.append(isotope_data, isotope_data,1)
-print (processed_isotope_data)
+# Append this new column to the existing temperature_data array
+processed_temperature_data = np.append(temperature_data, temperature_data,1)
+print (processed_temperature_data)
 
 # Create a figure of the processed data
-plt.style.use('seaborn-whitegrid')
-isotope_figure = plt.figure()
-isotope_plot = plt.scatter (processed_isotope_data[:,0], processed_isotope_data[:,2])
-plt.xlabel("Carbon")
-plt.ylabel("Oxygen")
-plt.title('Stable Isotope Analysis')
+temperature_figure = plt.figure()
+temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=30, color='green')
 plt.show(block=True)
-isotope_figure.savefig('results/stable-isotopes.pdf')
+temperature_figure.savefig('results/temperature-over-time.pdf')
 
 def convert_data(filename, output_filename):
     all_data = pd.read_csv(filename, index_col='Date', header=4)
@@ -41,9 +37,9 @@ def convert_data(filename, output_filename):
     plot_filename = os.path.join(results_directory,plot_file)
     json_filename = os.path.join(results_directory,json_output_file)
 
-    isotope_data = read_data(input_filename, starting_row=3)
-    processed_isotope_data = process_data(isotope_data)
-    plot_data(processed_isotope_data, plot_filename)
+    temperature_data = read_data(input_filename, starting_row=3)
+    processed_temperature_data = process_data(temperature_data)
+    plot_data(processed_temperature_data, plot_filename)
     convert_data(input_filename, json_filename)
 
 if __name__ == "__main__":
